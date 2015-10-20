c48961d756d29948939e49a91da366b39c522371
philip <philip@192.168.1.163>
2015-10-20 20:16:33 +0200

    Udkast til "T-shirt" klassen

---
+++ git_test/TShirt.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_test;
+
+/**
+ *
+ * @author philip
+ */
+public class TShirt {
+    private String navn;
+    private double pris;
+    private int[] størrelse;
+    private String[] farve;
+    
+    public TShirt(String navn, double pris, int[] størrelse, String[] farve) {
+        this.navn = navn;
+        this.pris = pris;
+        this.størrelse = størrelse;
+        this.farve = farve;
+    }
+}
