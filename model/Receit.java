8e5667326d5c3fc88c79a746cfee5f1ea7097f89
Simon <Simon@Simonfh-pc>
2015-10-29 22:55:12 +0100

    ALT KODE VIRKER :D

--- model/Receit.java
+++ model/Receit.java
@@ -5,11 +5,17 @@
  */
 package model;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author Simon
  */
 public class Receit {
+
     private String content;
 
     public Receit(String content) {
@@ -23,5 +29,23 @@ public class Receit {
     public void setContent(String content) {
         this.content = content;
     }
-    
+
+    public void printReceit() {
+        String indkøb = "";
+        String filename = "order.txt";
+        File fil = new File(filename);
+        PrintWriter pw;
+        try {
+            Scanner scan = new Scanner(content);
+            pw = new PrintWriter(fil);
+            while (scan.hasNext()) {
+                pw.println(scan.nextLine());
+            }
+            pw.close();
+        } catch (IOException ex) {
+            System.out.println("Det var ikke muligt at printe ordren: " + ex.getMessage());
+
+        }
+
+    }
 }
